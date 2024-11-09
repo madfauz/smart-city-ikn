69887f0bf6bfd7e7a6616af79d4c89066b6b9d4b
Ahmad Fauzi <ahmadfauzi8341@gmail.com>
2024-11-09 23:10:59 +0700

    config-tailwind-1

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ export default {
         secondary: "#FCF7F0",
         dark: "#080809",
       },
+      boxShadow: {
+        "custom-dark": "0 2px 54px rgba(170, 170, 170, 0.28)",
+      },
+      gradientColorStops: {
+        "start-primary": "#EEB25C",
+        "end-primary": "#F9CA86",
+      },
     },
   },
   plugins: [],
