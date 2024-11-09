96ac12c1f6b989ffeafe91d6acc7afb3cefd0a72
Ahmad Fauzi <ahmadfauzi8341@gmail.com>
2024-11-09 22:46:36 +0700

    coba

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,6 @@ export default {
         dark: "#080809",
         gray: "#666666",
       },
-      boxShadow: {
-        "custom-dark": "0 2px 54px rgba(170, 170, 170, 0.28)",
-      },
-      gradientColorStops: {
-        "start-primary": "#EEB25C",
-        "end-primary": "#F9CA86",
-      },
     },
   },
   plugins: [],
