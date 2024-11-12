e89a54e8cc6bca49fbd25fa2cc8b8f650258771d
Ahmad Fauzi <ahmadfauzi8341@gmail.com>
2024-11-12 12:20:20 +0700

    edit navbar dan project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -26,8 +26,8 @@ const Project = () => {
         className="flex flex-wrap xl:flex-nowrap flex-row gap-6 w-full"
       >
         <aside className="flex flex-col sm:flex-row xl:flex-col gap-6 basis-full xl:basis-4/12">
-          <div className="relative rounded-[10px] w-full sm:w-1/2 xl:w-auto xl:h-2/3">
-            <div className="bg-black opacity-30 absolute top-0 left-0 w-full h-full z-10 rounded-[10px]"></div>
+          <div className="relative rounded-[10px] w-full h-[240px] sm:h-auto sm:w-1/2 xl:w-auto xl:h-2/3">
+            <div className="bg-gradient-to-t from-black to-white opacity-40 absolute top-0 left-0 w-full h-full z-10 rounded-[10px]"></div>
             <img
               src={projectPlaza}
               className="relative rounded-[10px] w-full h-full object-cover"
@@ -46,8 +46,8 @@ const Project = () => {
               </p>
             </div>
           </div>
-          <div className="relative rounded-[10px] w-full sm:w-1/2 xl:w-auto xl:h-1/3">
-            <div className="bg-black opacity-30 absolute top-0 left-0 w-full h-full z-10 rounded-[10px] box-border"></div>
+          <div className="relative rounded-[10px] w-full h-[240px] sm:h-auto sm:w-1/2 xl:w-auto xl:h-1/3">
+            <div className="bg-gradient-to-t from-black to-white opacity-40 absolute top-0 left-0 w-full h-full z-10 rounded-[10px] box-border"></div>
             <img
               src={projectTaxi}
               className="relative rounded-[10px] w-full h-full object-cover"
@@ -68,8 +68,8 @@ const Project = () => {
           </div>
         </aside>
         <aside className="flex flex-col gap-6 basis-full h-2/3 xl:h-auto xl:basis-3/12">
-          <div className="relative rounded-[10px] h-full">
-            <div className="bg-black opacity-30 absolute top-0 left-0 w-full h-full z-10 rounded-[10px]"></div>
+          <div className="relative rounded-[10px] h-[240px] sm:h-[280px] lg:h-[320px] xl:h-full">
+            <div className="bg-gradient-to-t from-black to-white opacity-40 absolute top-0 left-0 w-full h-full z-10 rounded-[10px]"></div>
             <img
               src={projectMall}
               className="relative rounded-[10px] w-full h-full object-cover"
@@ -90,8 +90,8 @@ const Project = () => {
           </div>
         </aside>
         <aside className="flex flex-col sm:flex-row xl:flex-col gap-6 basis-full xl:basis-5/12">
-          <div className="relative rounded-[10px] w-full sm:w-1/2 xl:w-auto h-full xl:h-1/2">
-            <div className="bg-black opacity-30 absolute top-0 left-0 w-full h-full z-10 rounded-[10px]"></div>
+          <div className="relative rounded-[10px] w-full sm:w-1/2 xl:w-auto h-[240px] sm:h-full xl:h-1/2">
+            <div className="bg-gradient-to-t from-black to-white opacity-40 absolute top-0 left-0 w-full h-full z-10 rounded-[10px]"></div>
             <img
               src={projectCharging}
               className="relative rounded-[10px] w-full h-full object-cover"
@@ -110,8 +110,8 @@ const Project = () => {
               </p>
             </div>
           </div>
-          <div className="relative rounded-[10px] w-full sm:w-1/2 xl:w-auto h-full xl:h-1/2">
-            <div className="bg-black opacity-30 absolute top-0 left-0 w-full h-full z-10 rounded-[10px]"></div>
+          <div className="relative rounded-[10px] w-full sm:w-1/2 xl:w-auto h-[240px] sm:h-full xl:h-1/2">
+            <div className="bg-gradient-to-t from-black to-white opacity-40 absolute top-0 left-0 w-full h-full z-10 rounded-[10px]"></div>
             <img
               src={projectKereta}
               className="relative rounded-[10px] w-full h-full object-cover"
