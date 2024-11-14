fc1cd446e5dc1402832b5ed809ce085f004ebcb0
Ahmad Fauzi <ahmadfauzi8341@gmail.com>
2024-11-15 00:07:43 +0700

    penyesuaian font size project and desc

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -14,10 +14,11 @@ const Project = () => {
       id="proyek"
     >
       <section className="flex flex-col gap-2 ">
-        <h2 className="font-poppins font-bold text-[24px] md:text-[28px] text-center text-dark dark:text-secondary">
+        <h2 className="font-poppins font-bold text-vm-title sm:text-sm-title md:text-md-title lg:text-lg-title xl:text-xl-title text-center text-dark dark:text-secondary">
           {t("konten-proyek.judul")}
         </h2>
-        <p className="font-poppins font-normal text-[14px] sm:text-[16px] md:text-[20px] text-center text-gray dark:text-secondary">
+
+        <p className="font-poppins font-medium text-vm-desc sm:text-sm-desc md:text-md-desc lg:text-lg-desc xl:text-xl-desc text-center text-dark dark:text-secondary">
           {t("konten-proyek.deskripsi")}
         </p>
       </section>
@@ -31,15 +32,15 @@ const Project = () => {
               className="relative rounded-[10px] w-full h-full object-cover"
               alt="project-plaza"
             />
-            <span className="font-poppins font-semibold absolute z-20 top-0 left-0 px-4 py-2 text-[14px] sm:text-[16px] md:text-[20px] text-white bg-primary rounded-tl-[10px] rounded-br-[10px]">
+            <span className="font-poppins font-semibold absolute z-20 top-0 left-0 px-4 py-2 text-white bg-primary rounded-tl-[10px] rounded-br-[10px] text-vm-card-tag sm:text-sm-card-tag md:text-md-card-tag lg:text-lg-card-tag xl:text-xl-card-tag">
               {t("konten-proyek.tipe-proyek.proyek-1")}
             </span>
 
             <div className="absolute z-20 bottom-4 left-4 text-white">
-              <span className="font-poppins font-semibold text-[18px] sm:text-[20px]">
+              <span className="font-poppins font-semibold text-vm-card-title sm:text-sm-card-title md:text-md-card-title lg:text-lg-card-title xl:text-xl-card-title">
                 {t("konten-proyek.judul-proyek.proyek-1")}
               </span>
-              <p className="font-poppins font-light italic text-[14px] sm:text-[16px] line-clamp-2">
+              <p className="font-poppins font-normal italic text-vm-card-desc sm:text-sm-card-desc md:text-md-card-desc lg:text-lg-card-desc xl:text-xl-card-desc line-clamp-2">
                 {t("konten-proyek.deskripsi-proyek.proyek-1")}
               </p>
             </div>
@@ -51,15 +52,15 @@ const Project = () => {
               className="relative rounded-[10px] w-full h-full object-cover"
               alt="project-taxi"
             />
-            <span className="font-poppins font-semibold absolute z-20 top-0 left-0 px-4 py-2 text-[14px] sm:text-[16px] md:text-[20px] text-white bg-primary rounded-tl-[10px] rounded-br-[10px]">
+            <span className="font-poppins font-semibold absolute z-20 top-0 left-0 px-4 py-2 text-vm-card-tag sm:text-sm-card-tag md:text-md-card-tag lg:text-lg-card-tag xl:text-xl-card-tag text-white bg-primary rounded-tl-[10px] rounded-br-[10px]">
               {t("konten-proyek.tipe-proyek.proyek-2")}
             </span>
 
             <div className="absolute z-20 bottom-4 left-4 text-white">
-              <span className="font-poppins font-semibold text-[18px] sm:text-[20px]">
+              <span className="font-poppins font-semibold text-vm-card-title sm:text-sm-card-title md:text-md-card-title lg:text-lg-card-title xl:text-xl-card-title">
                 {t("konten-proyek.judul-proyek.proyek-2")}
               </span>
-              <p className="font-poppins font-light italic text-[14px] sm:text-[16px] line-clamp-2">
+              <p className="font-poppins font-normal italic text-vm-card-desc sm:text-sm-card-desc md:text-md-card-desc lg:text-lg-card-desc xl:text-xl-card-desc line-clamp-2">
                 {t("konten-proyek.deskripsi-proyek.proyek-2")}
               </p>
             </div>
@@ -73,15 +74,15 @@ const Project = () => {
               className="relative rounded-[10px] w-full h-full object-cover"
               alt="project-mall"
             />
-            <span className="font-poppins font-semibold absolute z-20 top-0 left-0 px-4 py-2 text-[14px] sm:text-[16px] md:text-[20px] text-white bg-primary rounded-tl-[10px] rounded-br-[10px]">
+            <span className="font-poppins font-semibold absolute z-20 top-0 left-0 px-4 py-2 text-vm-card-tag sm:text-sm-card-tag md:text-md-card-tag lg:text-lg-card-tag xl:text-xl-card-tag text-white bg-primary rounded-tl-[10px] rounded-br-[10px]">
               {t("konten-proyek.tipe-proyek.proyek-3")}
             </span>
 
             <div className="absolute z-20 bottom-4 left-4 text-white">
-              <span className="font-poppins font-semibold text-[18px] sm:text-[20px]">
+              <span className="font-poppins font-semibold text-vm-card-title sm:text-sm-card-title md:text-md-card-title lg:text-lg-card-title xl:text-xl-card-title">
                 {t("konten-proyek.judul-proyek.proyek-3")}
               </span>
-              <p className="font-poppins font-light italic text-[14px] sm:text-[16px] line-clamp-2">
+              <p className="font-poppins font-normal italic text-vm-card-desc sm:text-sm-card-desc md:text-md-card-desc lg:text-lg-card-desc xl:text-xl-card-desc line-clamp-2">
                 {t("konten-proyek.deskripsi-proyek.proyek-3")}
               </p>
             </div>
@@ -95,15 +96,15 @@ const Project = () => {
               className="relative rounded-[10px] w-full h-full object-cover"
               alt="project-charging"
             />
-            <span className="font-poppins font-semibold absolute z-20 top-0 left-0 px-4 py-2 text-[14px] sm:text-[16px] md:text-[20px] text-white bg-primary rounded-tl-[10px] rounded-br-[10px]">
+            <span className="font-poppins font-semibold absolute z-20 top-0 left-0 px-4 py-2 text-vm-card-tag sm:text-sm-card-tag md:text-md-card-tag lg:text-lg-card-tag xl:text-xl-card-tag text-white bg-primary rounded-tl-[10px] rounded-br-[10px]">
               {t("konten-proyek.tipe-proyek.proyek-4")}
             </span>
 
             <div className="absolute z-20 bottom-4 left-4 text-white">
-              <span className="font-poppins font-semibold text-[18px] sm:text-[20px]">
+              <span className="font-poppins font-semibold text-vm-card-title sm:text-sm-card-title md:text-md-card-title lg:text-lg-card-title xl:text-xl-card-title">
                 {t("konten-proyek.judul-proyek.proyek-4")}
               </span>
-              <p className="font-poppins font-light italic text-[14px] sm:text-[16px] line-clamp-2">
+              <p className="font-poppins font-normal italic text-vm-card-desc sm:text-sm-card-desc md:text-md-card-desc lg:text-lg-card-desc xl:text-xl-card-desc line-clamp-2">
                 {t("konten-proyek.deskripsi-proyek.proyek-4")}
               </p>
             </div>
@@ -115,15 +116,15 @@ const Project = () => {
               className="relative rounded-[10px] w-full h-full object-cover"
               alt="project-kereta"
             />
-            <span className="font-poppins font-semibold absolute z-20 top-0 left-0 px-4 py-2 text-[14px] sm:text-[16px] md:text-[20px] text-white bg-primary rounded-tl-[10px] rounded-br-[10px]">
+            <span className="font-poppins font-semibold absolute z-20 top-0 left-0 px-4 py-2 text-vm-card-tag sm:text-sm-card-tag md:text-md-card-tag lg:text-lg-card-tag xl:text-xl-card-tag text-white bg-primary rounded-tl-[10px] rounded-br-[10px]">
               {t("konten-proyek.tipe-proyek.proyek-5")}
             </span>
 
             <div className="absolute z-20 bottom-4 left-4 text-white">
-              <span className="font-poppins font-semibold text-[18px] sm:text-[20px]">
+              <span className="font-poppins font-semibold text-vm-card-title sm:text-sm-card-title md:text-md-card-title lg:text-lg-card-title xl:text-xl-card-title">
                 {t("konten-proyek.judul-proyek.proyek-5")}
               </span>
-              <p className="font-poppins font-light italic text-[14px] sm:text-[16px] line-clamp-2">
+              <p className="font-poppins font-normal italic text-vm-card-desc sm:text-sm-card-desc md:text-md-card-desc lg:text-lg-card-desc xl:text-xl-card-desc line-clamp-2">
                 {t("konten-proyek.deskripsi-proyek.proyek-5")}
               </p>
             </div>
